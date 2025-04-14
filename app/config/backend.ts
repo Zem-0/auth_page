f6222ea2378d12c3831605e91af4a58a4dd0ae01
Zem-0 <parinith99@gmail.com>
2025-04-14 21:38:49 +0530

    push

---
+++ app/config/backend.ts
@@ -0,0 +1,31 @@
+import EmailPasswordNode from "supertokens-node/recipe/emailpassword";
+import SessionNode from "supertokens-node/recipe/session";
+import Dashboard from "supertokens-node/recipe/dashboard";
+import UserRoles from "supertokens-node/recipe/userroles";
+import { appInfo } from "./appInfo";
+import { TypeInput } from "supertokens-node/types";
+import SuperTokens from "supertokens-node";
+
+export let backendConfig = (): TypeInput => {
+    return {
+        supertokens: {
+            // this is the location of the SuperTokens core.
+            connectionURI: "https://st-dev-4dba6070-1930-11f0-8f17-a7f732f4707d.aws.supertokens.io",
+            apiKey: "JU75b1zCQ9ffCohUV3b6Yd1nb0",
+        },
+        appInfo,
+        // recipeList contains all the modules that you want to
+        // use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
+        recipeList: [EmailPasswordNode.init(), SessionNode.init(), Dashboard.init(), UserRoles.init()],
+        isInServerlessEnv: true,
+        framework: "custom",
+    };
+};
+
+let initialized = false;
+export function ensureSuperTokensInit() {
+    if (!initialized) {
+        SuperTokens.init(backendConfig());
+        initialized = true;
+    }
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { HomePage } from "./components/home";
+import styles from "./page.module.css";
+
+export default function Home() {
+    return (
+        <main className={styles.main}>
+            <HomePage />
+            SuperTokens.init
+        </main>
+    );
+}
